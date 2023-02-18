1efa09e561b43447f1173e22378361b0c4d23c93
souleymane-Aziz <123738749+souleymane-Aziz@users.noreply.github.com>
2023-02-18 14:31:49 +0100

    TP-01: premier d'initiation

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
+
+const verif = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    const {
+        params: { op, a, b },
+    } = req;
+    var x: number = +a;
+    var y: number = +b;
+
+    if (
+      !Number.isNaN(x) &&
+      !Number.isNaN(y)
+    ) {
+        if (
+          op === "sum" ||
+          op === "sous" ||
+          op === "mult" ||
+          op === "div"
+        )
+            next();
+        else {
+            res.send(
+              "Error: you must choose sum or sous or mult ou div"
+            );
+        }
+    } else {
+        res.send(
+          "Error: a ou b not number"
+        );
+    }
+};
+
+const calcul = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    const {
+        params: { op, a, b },
+    } = req;
+    var x: number = +a;
+    var y: number = +b;
+    var s;
+
+    switch (op) {
+        case "sum":
+            res
+              .status(200)
+              .send(
+                `le resultat de l'operation ${op} de ${a} et ${b} est egale a ${x+y}`
+              );
+            break;
+        case "sous":
+            res
+              .status(200)
+              .send(
+                `le resultat de l'operation ${op} de ${a} et ${b} est egale a ${x-y}`
+              );
+            break;
+        case "mult":
+            res
+              .status(200)
+              .send(
+                `le resultat de l'operation ${op} de ${a} et ${b} est egale a ${x*y}`
+              );
+            break;
+        case "div":
+            res
+              .status(200)
+              .send(
+                `le resultat de l'operation ${op} de ${a} et ${b} est egale a ${x/y}`
+              );
+            break;
+        default:
+            break;
+    }
+
+};
+
+app.get("/calculs",verif,calcul);
+
+
+app.listen(9000, () =>{
+    console.log('server est en ecoute sur le port 9000');
+})
